682aebb9bd8e1733a0b0a2ffed5c47ed20af6464
LAPTOP-D40802RR\L-nic <L-nickname@mail.ru>
2021-07-19 17:48:45 +0300

    upd

--- landingpagewebsite/config/settings.py
+++ landingpagewebsite/config/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# 'django-insecure-5$e#5q10t+4oxypwy=b5#v%6k^m*mm4l^=(pp$fxt+os^&#spp'
 SECRET_KEY = SEC_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
